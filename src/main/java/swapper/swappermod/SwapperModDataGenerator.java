477accfb79c6ca79aa9a413be8b931e993383c00
rhonnielcastro8 <rhonnielcastro00@gmail.com>
2026-07-22 22:51:16 +0800

    Initial commit

---
+++ src/main/java/swapper/swappermod/SwapperModDataGenerator.java
@@ -0,0 +1,11 @@
+package swapper.swappermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SwapperModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
